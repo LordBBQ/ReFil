7fc968a88ad8325038ce92581cd6f830bf3cc21e
LordBBQ <46541720+LordBBQ@users.noreply.github.com>
2024-02-25 10:19:26 +1100

    uploaded code

---
+++ ReFilFW/ReFil.ino
@@ -0,0 +1,73 @@
+#include "Thermistors.h"
+#include "PIDController.h"
+
+//Heater 0 constants
+#define HEATER_0_KP 3
+#define HEATER_0_KI 100
+#define HEATER_0_KD 0.0
+
+//Heater 1 constants
+#define HEATER_1_KP HEATER_0_KP
+#define HEATER_1_KI HEATER_0_KI
+#define HEATER_1_KD HEATER_0_KD
+
+//Heater 2 constants
+#define HEATER_2_KP HEATER_0_KP
+#define HEATER_2_KI HEATER_0_KI
+#define HEATER_2_KD HEATER_0_KD
+
+//Heater 3 constants
+#define HEATER_3_KP HEATER_0_KP
+#define HEATER_3_KI HEATER_0_KI
+#define HEATER_3_KD HEATER_0_KD
+
+#define SERIAL_BAUDRATE 115200
+
+int dutyCycle;
+
+void setup() {
+   thermalSafteyHasInit = false;
+  // put your setup code here, to run once:
+  Serial.begin(SERIAL_BAUDRATE);  
+  initPIDController();
+  initThermalSaftey();
+  dutyCycle = 0;
+}
+
+void loop() {
+  double target = 240;
+  // put your main code here, to run repeatedly:
+//  double thermistorVoltage = ((analogRead(A7) / 1024.0) * 5.0);
+//  double R2 = (thermistorVoltage * R1) / (VIN - (thermistorVoltage));
+//  Serial.print(thermistorVoltage);
+//  Serial.println(" Volts");
+//  Serial.print(R2);
+//  Serial.println(" Ohms");
+//  delay(1000);
+  dutyCycle = PIDController(HEATER_0_KP, HEATER_0_KI, HEATER_0_KD, target, getThermistorValue(0), 50);
+
+  Serial.print("Thermistor:");
+  Serial.print(getThermistorValue(0));
+  Serial.print(",");
+  Serial.print("Output:");
+  Serial.print(dutyCycle);
+  Serial.print(",");
+  Serial.print("Target:");
+  Serial.println(target);
+  //analogWrite(13, 255);
+    Serial.print("saftey thresh:");
+    Serial.println(heaterFaultLevel[0]);
+  if(checkThermalSaftey(dutyCycle, getThermistorValue(0), 0) == true) {
+    //Serial.print("TRIP");
+  } else {
+
+  }
+
+  if(checkThermalSaftey(dutyCycle, getThermistorValue(0), 0) == true) {
+    analogWrite(13, 0);
+    Serial.print("TRIP!!!");
+  } else {
+    analogWrite(13, dutyCycle);
+  }
+  
+}
---
+++ ReFilFW/Thermistors.h
@@ -0,0 +1,165 @@
+
+#define THERMISTOR_0_PIN A7
+#define THERMISTOR_1_PIN A7 //TODO
+#define THERMISTOR_2_PIN A7 //TODO
+#define THERMISTOR_3_PIN A7 //TODO
+
+#define HEATER_MINTEMP 10 //degrees c
+#define HEATER_MAXTEMP 260 //degrees c
+
+#define THERMAL_SAFTEY_FAULT_LEVEL_THRESH 200 //how many fault points before a failure is presumed
+#define THERMAL_SAFTEY_FAULT_LEVEL_DEGREDATION 3 //how many fault points are lost per cycle
+#define THERMAL_SAFTEY_CYCLE 500 //ms
+#define THERMAL_RUNAWAY_MIN_GRADIENT_DUTYCYCLE 0
+#define THERMAL_RUNAWAY_MIN_GRADIENT_TEMPERATURE 0
+
+#define VIN 5.0 //Volts
+#define R1 4700 //Ohms
+#define NOMINAL_RESISTANCE 100000 //Nominal resistance, Ohms
+#define NOMINAL_RESISTANCE_TEMP 25 //Temperature of nominal resistance reading, degrees celcius
+#define SAMPLE_COUNT 5 //Number of samples
+#define BETA_COEFFICIENT 3950
+
+int heaterFaultLevel[4];
+int heaterPreviousTemperature[4];
+int heaterPreviousDutyCycle[4];
+int heaterCurrentTemperature[4];
+int heaterCurrentDutyCycle[4];
+
+boolean thermalSafteyTrip = false;
+boolean thermalSafteyCycleComplete;
+boolean thermalSafteyHasInit = false;
+
+unsigned long thermalSafteyCurrentTime;
+unsigned long thermalSafteyPreviousTime;
+
+void initThermalSaftey() {
+  heaterFaultLevel[0] = 0;
+  heaterFaultLevel[1] = 0;
+  heaterFaultLevel[2] = 0;
+  heaterFaultLevel[3] = 0;
+
+  thermalSafteyTrip = false;
+  thermalSafteyCycleComplete = true;
+  thermalSafteyCurrentTime = millis();
+  thermalSafteyPreviousTime = millis();
+  thermalSafteyHasInit = true;
+
+  heaterPreviousTemperature[0] = 0;
+  heaterCurrentTemperature[0] = 0;
+  heaterPreviousDutyCycle[0] = 0;
+  heaterCurrentDutyCycle[0] = 0;
+  heaterPreviousTemperature[1] = 0;
+  heaterCurrentTemperature[1] = 0;
+  heaterPreviousDutyCycle[1] = 0;
+  heaterCurrentDutyCycle[1] = 0;
+  heaterPreviousTemperature[2] = 0;
+  heaterCurrentTemperature[2] = 0;
+  heaterPreviousDutyCycle[2] = 0;
+  heaterCurrentDutyCycle[2] = 0;
+  heaterPreviousTemperature[3] = 0;
+  heaterCurrentTemperature[3] = 0;
+  heaterPreviousDutyCycle[3] = 0;
+  heaterCurrentDutyCycle[3] = 0;
+
+
+}
+/**
+ * @param byte heaterIndex: The index of the heater which's thermistor should be checked
+ */
+double calculateTemperature(double adcValue) {
+  double thermistorVoltage = ((adcValue / 1024.0) * VIN);
+  double thermistorResistance = (thermistorVoltage * R1) / (VIN - (thermistorVoltage));
+  double temperature = 1.0 / (
+    log(
+          thermistorResistance / NOMINAL_RESISTANCE
+        ) / BETA_COEFFICIENT + 1.0 / (
+           NOMINAL_RESISTANCE_TEMP + 273.15
+        )
+     );
+  return temperature - 273.15; //convert from K to C
+}
+
+unsigned char getThermistorPort(byte heaterIndex) {
+  switch (heaterIndex) {
+    case 0:
+      return THERMISTOR_0_PIN;
+    case 1:
+      return THERMISTOR_1_PIN;
+    case 2:
+      return THERMISTOR_2_PIN;
+    case 3:
+      return THERMISTOR_3_PIN;
+  }
+}
+ 
+double getThermistorValue(byte heaterIndex) {
+  return (
+    calculateTemperature(
+      analogRead(
+        getThermistorPort(heaterIndex)
+      )
+    )
+  );
+}
+
+/**
+ * Returns true if a thermal fault has occured
+ * @param double requestedDutyCycle: the requested heater duty cycle
+ * @param double feebackTemperature: the thermistor temperature
+ */
+boolean checkThermalSaftey(double requestedDutyCycle, double feedbackTemperature, byte heaterIndex) {
+  //Serial.print(thermalSafteyTrip && thermalSafteyHasInit);
+
+  if(heaterFaultLevel[heaterIndex] >= THERMAL_SAFTEY_FAULT_LEVEL_THRESH) { //Check if fault level is over threshold, if so trip protection.
+    return true;
+  } else { //if threshold is ok, continue to checks
+    if(thermalSafteyCycleComplete) { //check if cycle has elapsed, if so run checks
+      Serial.print("trig");
+
+      
+      heaterCurrentTemperature[heaterIndex] = feedbackTemperature;
+      heaterCurrentDutyCycle[heaterIndex] = requestedDutyCycle;
+      
+      if(feedbackTemperature <= HEATER_MINTEMP) {
+        heaterFaultLevel[heaterIndex] = 100 + heaterFaultLevel[heaterIndex];
+        //Serial.print("mintemp");
+      }
+      if(feedbackTemperature >= HEATER_MAXTEMP) {
+        heaterFaultLevel[heaterIndex] = 100 + heaterFaultLevel[heaterIndex];
+        //Serial.print("maxtemp");
+
+      }
+
+      if(heaterCurrentDutyCycle[heaterIndex] > THERMAL_RUNAWAY_MIN_GRADIENT_DUTYCYCLE && heaterPreviousDutyCycle[heaterIndex] > THERMAL_RUNAWAY_MIN_GRADIENT_DUTYCYCLE) {
+        if((heaterCurrentTemperature[heaterIndex] - heaterPreviousTemperature[heaterIndex]) < THERMAL_RUNAWAY_MIN_GRADIENT_TEMPERATURE) {
+          heaterFaultLevel[heaterIndex] = 9 + heaterFaultLevel[heaterIndex];
+        }
+      }
+
+      if(heaterFaultLevel[heaterIndex] <= 0) {
+        heaterFaultLevel[heaterIndex] = 0;
+      } else {
+        heaterFaultLevel[heaterIndex] -= THERMAL_SAFTEY_FAULT_LEVEL_DEGREDATION;
+      }
+
+      //
+      thermalSafteyCycleComplete = false;
+      heaterPreviousTemperature[heaterIndex] = heaterCurrentTemperature[heaterIndex];
+      heaterPreviousDutyCycle[heaterIndex] = heaterCurrentDutyCycle[heaterIndex];
+      
+      
+      thermalSafteyCurrentTime = millis();
+      thermalSafteyPreviousTime = thermalSafteyCurrentTime;
+    } else { //if has not has elapsed but not been checked, set flag to true so next execution runs checks
+      if((thermalSafteyCurrentTime - thermalSafteyPreviousTime) >= THERMAL_SAFTEY_CYCLE) {
+        thermalSafteyCycleComplete = true;
+      } else { //if time has not elapsed, update the current time
+        thermalSafteyCurrentTime = millis();
+      }
+    }
+    
+    
+  }
+  return false;
+}
